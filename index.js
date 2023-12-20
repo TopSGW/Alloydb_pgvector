81b4348103ab79af1d513d52a4a636718cdaae2d
gripindo <daniil.nikolaev@tempello.ai>
2023-12-20 13:44:57 -0800

    first_init

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
-const rows = await process.postgresql.query('SELECT id FROM matters');
-console.log(rows);
+import express from 'express';
+import postgresql from './postgresql.js';
+
+postgresql()
+
+const app = express();
+
+app.post('/get_matterId', async (req, res) => {
+  const rows = await process.postgresql.query('SELECT id FROM matters');
+  res.status(200).send(JSON.stringify(rows));
+});
+
+app.listen(3000, () => {
+  console.log('Server running on 3000');
+});
